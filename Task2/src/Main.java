0c95484c15f30958c2533caba535b4ec744bde3b
profitweb <chezsimo@yandex.ru>
2023-07-08 21:37:32 +0300

    Tape text without symbols & every word in one line !

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        //todo  код
+    public static void main (String[] args) {
+        System.out.println("Please enter your text!");
+        String text= new Scanner(System.in).nextLine();
+        System.out.println(method(text));
     }
 
-    //todo метод
-
+    public static String method (String text) {
+        text = text.replaceAll("[.,:;/'?!+-]", "");
+        return text= text.replaceAll("\\s+","\n");
+    }
 }
