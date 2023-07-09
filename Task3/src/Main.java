173a5acf48789e2d899eea46fa36d506b4132f22
profitweb <chezsimo@yandex.ru>
2023-07-09 17:04:14 +0300

    FIO with regex

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final String VALID_REGEX = "((([А-я]+)|([А-я-]+))\\s){2}(([А-я]+)|([А-я-]+))";
     public static void main(String[] args) {
-        //todo ФИО
+        System.out.println("Введите ФИО !");
+        String fio = new Scanner(System.in).nextLine();
+
+        if (fio.matches(VALID_REGEX)) {
+            System.out.println(true);
+        }
+            else {
+                System.out.println(false);
+            }
+
     }
 }
