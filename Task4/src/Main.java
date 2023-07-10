e33dfda8aab3ac7078951c3486f7407b6367eeab
profitweb <chezsimo@yandex.ru>
2023-07-10 03:12:03 +0300

    Correct number !

--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final String NUM_REGEX = "(\\d{3})(\\d{3})(\\d{2})(\\d{2})";
+    private static final String NUM_LEGAL = "+7 ($2) $3 $4-$5";
+
+    public static final String  SHORT_NUM_LEGAL = "+7 ($1) $2 $3-$4";
     public static void main(String[] args) {
-        //todo Numbers
+        while(true) {
+            System.out.println("Please Enter your phone number!");
+            String num = new Scanner(System.in).nextLine();
+            num = num.replaceAll("\\D+","");
+
+            if (num.length() == 11) {
+                num = num.replaceAll("(\\d)" + NUM_REGEX, NUM_LEGAL);
+                System.out.println(num);
+            }
+            else if (num.length() == 10) {
+                num = num.replaceAll(NUM_REGEX, SHORT_NUM_LEGAL);
+                System.out.println(num);
+            }
+            else {
+                System.out.println("Sorry! number is not correct!");
+            }
+        }
     }
 }
